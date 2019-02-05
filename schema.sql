49438c6c5d06fd59411f70ba1c3739b9c554a004
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-02-05 23:23:39 +0300

    upgrade db schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,39 @@
 begin;
 
+-- @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+-- @version 1.000
+-- @date    2019-02-05
 
 create table asso (
   id bigserial not null primary key,
   name character varying(255) not null,
   asso character varying(255) not null,
+  created timestamp(0) without time zone not null default now(),
   counter bigint not null default 1,
   checked bool not null default false,
   unique(name,asso)
 );
 
+comment on table asso is 'таблица с ассоциациями';
+
+comment on column asso.id is 'id записи';
+comment on column asso.name is 'слово, к которому придумываем оссоциации';
+comment on column asso.asso is 'ассоциация';
+comment on column asso.created is 'дата создания ассоциации';
+comment on column asso.counter is 'сколько раз ассоциация использовалась';
+comment on column asso.checked is 'флаг того, что ассоциация проверена';
+
 create table words (
   id bigserial not null primary key,
   name character varying(255) not null,
+  created timestamp(0) without time zone not null default now(),
   unique(name)
 );
 
-end;
+comment on table words is 'таблица слов для начала игры';
 
+comment on column words.id is 'id слова';
+comment on column words.name is 'слово';
+comment on column words.created is 'время создания';
+
+end;
