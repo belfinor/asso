d9635be7dfe7621e731532e3567b392832402b03
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-02-05 17:08:05 +0300

    skeleton

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2019-02-05
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/belfinor/Helium/daemon"
+	"github.com/belfinor/Helium/log"
+	"github.com/belfinor/Helium/net/jsonrpc2"
+)
+
+type Config struct {
+	Daemon   daemon.Config `json:"daemon"`
+	Database struct {
+		Driver  string `json:"driver"`
+		Connect string `json:"connect"`
+	} `json:"database"`
+	Server jsonrpc2.HttpConfig `json:"server"`
+	Log    log.Config          `json:"log"`
+	GC     int                 `json:"gc"`
+}
+
+var _config *Config
+
+func Load(filename string) *Config {
+
+	data, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(fmt.Sprintf("Open file %s error", filename))
+	}
+
+	var _con Config
+
+	err = json.Unmarshal(data, &_con)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &_con
+}
+
+func Set(conf *Config) {
+	_config = conf
+}
+
+func Init(conf string) *Config {
+	if _config == nil {
+		_config = Load(conf)
+	}
+
+	return _config
+}
+
+func Get() *Config {
+	return _config
+}
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2019-02-05
+
+import (
+	"flag"
+
+	"github.com/belfinor/Helium/daemon"
+	"github.com/belfinor/Helium/log"
+	"github.com/belfinor/asso/config"
+)
+
+func main() {
+	conf := ""
+	is_daemon := false
+
+	flag.StringVar(&conf, "c", "asso.json", "config file name")
+	flag.BoolVar(&is_daemon, "d", false, "run as daemon")
+
+	flag.Parse()
+
+	cfg := config.Init(conf)
+
+	if is_daemon {
+		daemon.Run(&cfg.Daemon)
+	}
+
+	log.Init(&cfg.Log)
+
+	if is_daemon {
+		log.Info("start application as daemon")
+	} else {
+		log.Info("start application")
+	}
+
+}
